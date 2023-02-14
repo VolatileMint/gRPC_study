0b7f26e481f15a78fd1726df5f71ca39c0f0d9e2
VolatileMint <e203take@gmial.com>
2023-02-14 12:24:31 +0900

    chapter09 fin

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"os"
@@ -43,6 +45,25 @@ func (s *myServer) HelloServerStream(req *hellopb.HelloRequest, stream hellopb.G
 		time.Sleep(time.Second * 1)
 	}
 	return nil
+}
+
+// Client Stream RPCがリクエストを受け取るところ
+func (s *myServer) HelloClientStream(stream hellopb.GreetingService_HelloClientStreamServer) error {
+	nameList := make([]string, 0)
+	for {
+		// stream のRecvメソッドを呼び出してリクエスト内容を取得する
+		req, err := stream.Recv()
+		if errors.Is(err, io.EOF) {
+			message := fmt.Sprintf("Hello, %v!", nameList)
+			return stream.SendAndClose(&hellopb.HelloResponse{
+				Message: message,
+			})
+		}
+		if err != nil {
+			return err
+		}
+		nameList = append(nameList, req.GetName())
+	}
 }
 
 // 自作サービス構造体のコンストラクタを定義
